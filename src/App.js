ea4f0838a6535af7b1c346fff61c39130d2be52c
mayur <mayurk@utterwise.tech>
2024-10-16 20:26:48 +0530

    Implemented background component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import Background from "./components/Background";
 
-function App() {
-  return <h1 className="text-3xl bg-blue-400 font-bold">Hello world!</h1>;
+const App = () => {
+  return (
+    <>
+      <Background />
+    </>
+  )
 }
 
 export default App;
